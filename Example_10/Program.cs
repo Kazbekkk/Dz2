76789fceba59c4b4205fb72ea508e45088152ed0
Kazbek <bender.rodrigiz2014@gmail.com>
2022-12-19 23:56:03 +0300

    Нахождение второго числа из трех значного числа

---
+++ Example_10/Program.cs
@@ -0,0 +1,6 @@
+﻿int RandomNum = new Random().Next(100, 1000); // Задаем диапазон от 100 до 999
+Console.WriteLine($"Рандомное число от 100 до 1000 будет {RandomNum}");
+string num = Convert.ToString(RandomNum); // Конвертируем число из int в string
+var TwoNumStr = num[1]; // Берем из строки 2 значение 
+int TwoNumInt = TwoNumStr - '0'; // Преобразование var (не явный тип данных) в int 
+Console.Write($"Вторая цифра рандомного числа {RandomNum} будет {TwoNumInt}"); // Вывод
