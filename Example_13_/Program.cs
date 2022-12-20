d8615adfa8c5f3302bb3df27a40cabefc74f1613
Kazbek <bender.rodrigiz2014@gmail.com>
2022-12-20 11:45:13 +0300

    Программа, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.

---
+++ Example_13_/Program.cs
@@ -0,0 +1,14 @@
+﻿int RandomNum = new Random().Next(10, 150); // Задаем диапазон от 10 до 9999
+Console.WriteLine($"Рандомное число от 10 до 9999 будет {RandomNum}");
+if (RandomNum < 100)
+{
+    Console.WriteLine($"В {RandomNum} нет 3-го числа");
+}
+
+else
+{
+    string num = Convert.ToString(RandomNum); // Конвертируем число из int в string
+    var TwoNumStr = num[2]; // Берем из строки 2 значение 
+    int TwoNumInt = TwoNumStr - '0'; // Преобразование var (не явный тип данных) в int 
+    Console.Write($"Третья цифра рандомного числа {RandomNum} будет {TwoNumInt}"); // Вывод
+}
