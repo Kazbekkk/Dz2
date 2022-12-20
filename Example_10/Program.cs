53dd639efff1ef78d0ecdbeec93fa6dac5657827
Kazbek <bender.rodrigiz2014@gmail.com>
2022-12-20 18:57:08 +0300

    Добавлено нахождение 2-го числа для отрицатеьных чисел(изменил)

--- Example_10/Program.cs
+++ Example_10/Program.cs
@@ -1,24 +1,35 @@
-﻿int RandomNum = new Random().Next(-1000, 1000); // Задаем диапазон от 100 до 999
+﻿int RandomNum = new Random().Next(-100, 101); // Задаем диапазон от -100 до 100
 Console.WriteLine($"Рандомное число от 100 до 1000 будет {RandomNum}");
-if (RandomNum < -10 | RandomNum < 10)
+if (RandomNum < 0)
 {
-    Console.WriteLine($"Число {RandomNum} не имеет 2-й цифры");
+    if (RandomNum > -10)
+    {
+        Console.WriteLine($"Число {RandomNum} не имеет 2-й цифры");
+    }
+
+    else
+    {
+
+        string num = Convert.ToString(RandomNum); // Конвертируем число из int в string
+        var TwoNumStr = num[2]; // Берем из строки 2 значение 
+        int TwoNumInt = TwoNumStr - '0'; // Преобразование var (не явный тип данных) в int 
+        Console.Write($"Вторая цифра рандомного числа {RandomNum} будет {TwoNumInt * -1}"); // Вывод
+
+    }
 }
+
 else
 {
-    if (RandomNum >= 0)
+    if (RandomNum < 10)
     {
-        string num = Convert.ToString(RandomNum); // Конвертируем число из int в string
-        var TwoNumStr = num[1]; // Берем из строки 2 значение 
-        int TwoNumInt = TwoNumStr - '0'; // Преобразование var (не явный тип данных) в int 
-        Console.Write($"Вторая цифра рандомного числа {RandomNum} будет {TwoNumInt}"); // Вывод
+        Console.WriteLine($"Число {RandomNum} не имеет 2-й цифры");
     }
 
     else
     {
         string num = Convert.ToString(RandomNum); // Конвертируем число из int в string
-        var TwoNumStr = num[2]; // Берем из строки 2 значение 
+        var TwoNumStr = num[1]; // Берем из строки 2 значение 
         int TwoNumInt = TwoNumStr - '0'; // Преобразование var (не явный тип данных) в int 
-        Console.Write($"Вторая цифра рандомного числа {RandomNum} будет {TwoNumInt * -1}"); // Вывод
+        Console.Write($"Вторая цифра рандомного числа {RandomNum} будет {TwoNumInt}"); // Вывод
     }
 }
