679d9ef385742bb1bf3ce219e94d479317c1a111
Kazbek <bender.rodrigiz2014@gmail.com>
2022-12-20 22:04:18 +0300

    Изменения (забыл написать git add)

--- Example_14/Program.cs
+++ Example_14/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Сегодня Декабрь 2022г. Какое сегодня число?");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if    (number == 3 || number == 4 
+    || number == 10 || number == 11 
+    || number == 17 || number == 18 
+    || number == 24 || number == 25 
+    || number == 31 )
+{
+    Console.WriteLine("Этот день Выходной");
+}
+else
+{
+    Console.WriteLine("Солнце еще высоко, работай негр");
+}
