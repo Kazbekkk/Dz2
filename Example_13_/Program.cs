d85c1bf4f51f1b7be080372f6241083005f47030
Kazbek <bender.rodrigiz2014@gmail.com>
2022-12-20 19:12:19 +0300

    Нахождение 3-го числа из отрицительных значений

--- Example_13_/Program.cs
+++ Example_13_/Program.cs
@@ -1,14 +1,33 @@
-﻿int RandomNum = new Random().Next(10, 150); // Задаем диапазон от 10 до 150
-Console.WriteLine($"Рандомное число от 10 до 150 будет {RandomNum}");
-if (RandomNum < 100)
+﻿int RandomNum = new Random().Next(-150, 150); // Задаем диапазон от -150 до 150
+Console.WriteLine($"Рандомное число от -150 до 150 будет {RandomNum}");
+if (RandomNum < 0) // Проверка на отрицательное число
 {
-    Console.WriteLine($"В {RandomNum} нет 3-го числа");
+    if (RandomNum > -100)
+    {
+        Console.WriteLine($"В {RandomNum} нет 3-го числа");
+    }
+
+    else
+    {
+        string num = Convert.ToString(RandomNum); // Конвертируем число из int в string
+        var TwoNumStr = num[3]; // Берем из строки 3 значение 
+        int TwoNumInt = TwoNumStr - '0'; // Преобразование var (не явный тип данных) в int 
+        Console.Write($"Третья цифра рандомного числа {RandomNum} будет {TwoNumInt * -1}"); // Вывод
+    }
 }
 
 else
 {
-    string num = Convert.ToString(RandomNum); // Конвертируем число из int в string
-    var TwoNumStr = num[2]; // Берем из строки 3 значение 
-    int TwoNumInt = TwoNumStr - '0'; // Преобразование var (не явный тип данных) в int 
-    Console.Write($"Третья цифра рандомного числа {RandomNum} будет {TwoNumInt}"); // Вывод
+    if (RandomNum < 100)
+    {
+        Console.WriteLine($"В {RandomNum} нет 3-го числа");
+    }
+
+    else
+    {
+        string num = Convert.ToString(RandomNum); // Конвертируем число из int в string
+        var TwoNumStr = num[2]; // Берем из строки 3 значение 
+        int TwoNumInt = TwoNumStr - '0'; // Преобразование var (не явный тип данных) в int 
+        Console.Write($"Третья цифра рандомного числа {RandomNum} будет {TwoNumInt}"); // Вывод
+    }
 }
